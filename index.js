309d8b2bc0249612b853ec49eaf2a9fee87df2bb
Tai Bui <buihtai94@gmail.com>
2019-10-31 17:12:47 +0700

    rewrite with react hook and fix bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { AppState, NativeModules } from "react-native";
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
 const { CheckUpdate } = NativeModules;
 
@@ -9,116 +9,105 @@ export const CURRENT_VERSION = CheckUpdate.currentVersion;
 export const CURRENT_BUILD = "" + CheckUpdate.currentBuild;
 
 export const Status = {
-    DEPRECATED: "DEPRECATED",
-    SUPPORTED: "SUPPORTED",
-    LATEST: "LATEST"
+  DEPRECATED: "DEPRECATED",
+  SUPPORTED: "SUPPORTED",
+  LATEST: "LATEST"
 };
 
-type Props = {
-    getSupportedBuilds: () => Array<any>,
-    minimumInterval?: number,
-    whenAppDeprecated: () => void,
-    whenAppSupported?: () => void,
-    whenAppLatest?: () => void
-};
-
-const DEFAULT_MINIMUM_INTERVAL = 60 * 60 * 1000; // one hour
 const KEY_LAST_PROMPT_TO_UPDATE = "last_prompt_to_update";
 
-function sortBuilds(e1, e2) {
-    const e1List = e1.split(".");
-    const e2List = e2.split(".");
-    while (e2List.length !== e1List.length) {
-        (e1List.length > e2List.length ? e2List : e1List).push("0");
+const sortBuilds = (e1, e2) => {
+  const e1List = e1.split(".");
+  const e2List = e2.split(".");
+  while (e2List.length !== e1List.length) {
+    (e1List.length > e2List.length ? e2List : e1List).push("0");
+  }
+  for (let i = 0; i < e1List.length; i++) {
+    if (parseInt(e1List[i]) > parseInt(e2List[i])) {
+      return 1;
+    } else if (parseInt(e1List[i]) < parseInt(e2List[i])) {
+      return -1;
     }
-    for (let i = 0; i < e1List.length; i++) {
-        if (parseInt(e1List[i]) > parseInt(e2List[i])) {
-            return 1
-        } else if (parseInt(e1List[i]) < parseInt(e2List[i])) {
-            return -1
-        }
-    }
-    return 0;
-}
+  }
+  return 0;
+};
 
-export class UpdateChecker extends Component<Props> {
+const classifyCurrentBuild = (supportedBuilds, betaBuilds) => {
+  if (
+    Array.isArray(betaBuilds) &&
+    betaBuilds.map(value => `${value}`).includes(CURRENT_BUILD)
+  ) {
+    return Status.LATEST;
+  }
+  const sortedList = supportedBuilds.map(value => `${value}`).sort(sortBuilds);
+  const index = sortedList.indexOf(CURRENT_BUILD);
+  if (index < 0) {
+    return Status.DEPRECATED;
+  } else if (index === sortedList.length - 1) {
+    return Status.LATEST;
+  } else {
+    return Status.SUPPORTED;
+  }
+};
 
-    constructor(props) {
-        super(props)
-    }
-
-    componentDidMount(): void {
-        AppState.addEventListener('change', this.handleAppStateChange);
-        this.checkForUpdate();
-    }
-
-    componentWillUnmount(): void {
-        AppState.removeEventListener('change', this.handleAppStateChange);
-    }
-
-    handleAppStateChange = (nextAppState) => {
-        if (nextAppState === "active") {
-            this.checkForUpdate();
-        }
-    };
-
-    checkForUpdate = async () => {
-        const { getSupportedBuilds } = this.props;
-        try {
-            const supportedBuilds = await getSupportedBuilds();
-            const status = this.classifyCurrentBuild(supportedBuilds);
-            this.onResult(status);
-        } catch (e) {
-            console.log("UpdateChecker", e.message)
-        }
-    };
-
-    classifyCurrentBuild = (supportedVersions) => {
-        const sortedList = supportedVersions.map(value => `${value}`)
-            .sort(sortBuilds);
-        const index = sortedList.indexOf(CURRENT_BUILD);
-        if (index < 0) {
-            return Status.DEPRECATED;
-        } else if (CURRENT_BUILD === sortedList[sortedList.length - 1]) {
-            return Status.LATEST;
-        } else {
-            return Status.SUPPORTED;
-        }
-    };
-
-    onResult = (status) => {
-        switch (status) {
-            case Status.DEPRECATED:
-                this.whenAppDeprecated();
-                break;
-            case Status.SUPPORTED:
-                this.whenAppSupported();
-                break;
-            case Status.LATEST:
-                this.whenAppLatest();
-                break;
-        }
-    };
-
-    whenAppDeprecated = () => {
-        this.props.whenAppDeprecated?.();
-    };
-
-    whenAppSupported = () => {
-        const { whenAppSupported, minimumInterval = DEFAULT_MINIMUM_INTERVAL } = this.props;
-        AsyncStorage.getItem(KEY_LAST_PROMPT_TO_UPDATE).then(value => {
-            if (!value || new Date().getTime() - parseInt(value) > minimumInterval) {
-                AsyncStorage.setItem(KEY_LAST_PROMPT_TO_UPDATE, new Date().getTime().toString());
-                whenAppSupported?.();
+export const UpdateChecker = props => {
+  const {
+    getSupportedBuilds,
+    whenAppDeprecated,
+    whenAppSupported,
+    whenAppLatest,
+    minimumInterval
+  } = props;
+  useEffect(() => {
+    onResult = status => {
+      switch (status) {
+        case Status.DEPRECATED:
+          whenAppDeprecated?.();
+          break;
+        case Status.SUPPORTED:
+          AsyncStorage.getItem(KEY_LAST_PROMPT_TO_UPDATE).then(value => {
+            if (!value || Date.now() - parseInt(value) > minimumInterval) {
+              AsyncStorage.setItem(
+                KEY_LAST_PROMPT_TO_UPDATE,
+                Date.now().toString()
+              );
+              whenAppSupported?.();
             }
-        });
+          });
+          break;
+        case Status.LATEST:
+          whenAppLatest?.();
+          break;
+      }
     };
-
-    whenAppLatest = () => {
-        this.props.whenAppLatest?.();
+    const checkForUpdate = async () => {
+      try {
+        const {
+          supportedBuilds = [],
+          betaBuilds = []
+        } = await getSupportedBuilds();
+        const status = classifyCurrentBuild(supportedBuilds, betaBuilds);
+        onResult(status);
+      } catch (e) {
+        console.log("UpdateChecker", e.message);
+      }
     };
+    const handleAppStateChange = nextAppState => {
+      if (nextAppState === "active") checkForUpdate();
+    };
+    AppState.addEventListener("change", handleAppStateChange);
+    return () => AppState.removeEventListener("change", handleAppStateChange);
+  }, [
+    getSupportedBuilds,
+    minimumInterval,
+    whenAppDeprecated,
+    whenAppLatest,
+    whenAppSupported
+  ]);
+  return null;
+};
 
-    render() {
-        return null;
-    }
-}
+UpdateChecker.defaultProps = {
+  getSupportedBuilds: () => [],
+  minimumInterval: 60 * 60 * 1000 // one hour
+};
